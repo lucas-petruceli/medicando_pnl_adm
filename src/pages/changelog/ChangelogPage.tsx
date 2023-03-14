c91665308344c6a1b8b67076ab4a45cb1374ee3f
lucas petruceli mattos <lucaspmtt@gmail.com>
2023-03-14 12:50:14 -0300

    adicionando chamada de rede na pagina changelogpage

--- src/pages/changelog/ChangelogPage.tsx
+++ src/pages/changelog/ChangelogPage.tsx
@@ -3,16 +3,29 @@ import React, { useState, useEffect } from 'react'
 type Props = {}
 
 const ChangelogPage = (props: Props) => {
+  const [itens, setItens] = useState([])
+
   let count = 0
   console.log(`ChangeLogPage ${count++}`)
 
   useEffect(() => {
     console.log(`useEffect ${count++}`)
-  })
+    fetch("https://jsonplaceholder.typicode.com/todos/")
+    .then((response) => response.json())
+    .then((data) => setItens(data))
+    .catch((error) => console.log(error))
+
+
+  }, [])
 
   return (
     <div>
       ChangelogPage
+      <ul>
+        {itens.map((it) => (
+          <li key={it['id']}> {it['title']}</li>
+        ))}
+      </ul>
     </div>
   )
 }
